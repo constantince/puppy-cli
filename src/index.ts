a6e3e8a935541fe63eff8ba75d4124bc948b1afa
陈焱 <alberteinstein007@126.com>
2020-11-02 18:08:40 +0800

    feat: New dependencies added.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-const puppy = 'Hello World';
+#!/usr/bin/env node
 
-console.log(puppy);
+// const { Command } = require('commander');
+// const chalk = require('chalk');
+// const { bgGreenBright } = require('chalk');
+import { Command } from 'commander';
+import Chalk from 'chalk';
+import Spawn from 'cross-spawn';
+// cosnt execa = require("execa");
+// const Spawn = require("cross-spawn")
+// console.log(chalk.red.bold.bgWhite('Hello World'));
+const program = new Command();
+// program.version('0.0.1');
+Spawn.sync('yo', ['puppy'], { stdio: 'inherit' });
+console.log(Chalk.red.bold.bgGreenBright('Done...'));
+Spawn.sync('rollup', ['-c', '-w'], { stdio: 'inherit' });
+
+program
+  .option('-create, --yeoman <y>', 'Create a yeoman project')
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+ 
+program.parse(process.argv);
+
+// (async () => {
+//   const {stdout} = await execa('echo', ['unicorns']);
+//   console.log(stdout);
+//   //=> 'unicorns'
+// })();
+ 
+// if (program.debug) console.log(chalk.red.bold.bgGreenBright(program.pizzaType));
+//   console.log('pizza details:', program.yeoman);
+// if (program.small) console.log('- small pizza size');
+// if (program.pizzaType) console.log(`- ${program.pizzaType}`);
