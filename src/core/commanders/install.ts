bfb8794b0a596151f5008561e52a4af3348d5cae
陈焱 <alberteinstein007@126.com>
2020-11-27 10:56:43 +0800

    feat: new feature added

--- src/core/commanders/install.ts
+++ src/core/commanders/install.ts
@@ -1,21 +1,29 @@
-import { CheckoutMoudles } from "../check_modules";
+import { CheckoutPlugin } from "../check_modules";
 import { CreateCmdList } from "../../types/types";
 import Spawn from "cross-spawn";
 import path from "path";
 import osenv from "osenv";
-
+//generator-puppy-react
 const PREFIX = 'puppy-plugins-';
 
+//puppy -i install react
 
-const install = (pluginType: CreateCmdList, pluginName: string): void => {
-    console.log(`puppy-plugins-${pluginName}`, pluginType);
-    CheckoutMoudles(pluginType, `puppy-plugins-${pluginName}`, ).then((exist: boolean) => {
+const install = (pluginName: string): void => {
+    console.log(`${pluginName}`, 'pluginType');
+    CheckoutPlugin(`${pluginName}`).then((exist: boolean) => {
         if(exist) {
             return console.log('Module has been installed.')
         }
         process.chdir(path.join(osenv.home(), '.puppy/'));
-        Spawn.sync('npm', ['install', `puppy-plugins-${pluginName}`, '-D'], { stdio: 'inherit' });
-        console.log(`Module puppy-plugins-${pluginName} installed.`)
+        Spawn.sync('npm', ['install', `${pluginName}`, '-D'], { stdio: 'inherit' });
+
+
+        //write cmd .yml
+
+        const module = require(path.join(osenv.home(), '.puppy/', 'node_modules', pluginName, 'index.js'));
+
+        module.call(null);
+
     })
 }
 
